1be973cd6ba4e8aa4c4f6f4f2c5fb6fc84471865
banecogic <bane93@gmail.com>
2021-07-20 22:30:56 +0200

    Initial Commit

---
+++ public/index.js
@@ -0,0 +1,88 @@
+const $content = document.getElementById("content");
+const $btnsPrev = document.getElementById("prev-pages");
+const $btnsNext = document.getElementById("next-pages");
+
+let page = 1;
+let cap = 1;
+
+function makeHTML(data){
+            let html = "";
+            data.forEach(element => {
+                html+=`<div>${element.name} loves ${element.food}</div>` 
+            });
+            return html;
+        }
+        document.getElementById("error").innerHTML = "...loading...";
+        function fetchData(page){
+            fetch(`http://localhost:3000/api/${page}`,{
+            method:"GET",
+            headers:{
+                "Accept":"application/json"
+            }
+        }).then(response=>response.json()).then(data=>{
+            document.getElementById("current-page").innerHTML = `Current Page: ${page}`;
+            $btnsPrev.innerHTML="";
+            $btnsNext.innerHTML="";
+            cap = data.size;
+            console.log(typeof page,typeof cap);
+            
+            for(let i=-1,count=0;page + i > 1 - 1;i--,count++){
+                if(count === 3) break;
+                $btnsPrev.insertAdjacentHTML("afterbegin",
+                `<button onclick="handleClick(${i})" >${page+i}</button>`)
+            }
+            for(let i=1,count=0;page +i < cap + 1;i++,count++){
+                if(count === 3) break;
+                $btnsNext.insertAdjacentHTML("beforeend",
+                `<button onclick="handleClick(${i})" >${page+i}</button>`)
+            }
+            document.getElementById("error").innerHTML = "";
+            $content.innerHTML=makeHTML(data.list);
+
+        }).catch(err=>document.getElementById("error").innerHTML = err);
+        }
+        fetchData(page);
+        function handleClick(arg){
+            switch(arg){
+                case 1:
+                    if(page != cap) page += arg;
+                    break;
+                case -1:
+                    if(page != 1) page += arg;
+                    break;
+                case -3:
+                case -2:
+                case 2:
+                case 3:
+                    page += arg;
+                    break;
+                case 'last':
+                    page = cap;
+                    break;
+                case 'first':
+                    page = 1;
+                
+            }
+        fetchData(page);
+        }
+
+        function handleSubmit(e){
+            e.preventDefault();
+            const name = document.getElementById("name").value;
+            if(!name) return;
+            document.getElementById("error").innerHTML = "...loading...";
+            fetch("http://localhost:3000/name",{
+                method:"POST",
+                headers:{
+                    'Content-Type':"application/json"
+                },
+                body:JSON.stringify({name})
+            }).then((response)=>response.json()).then(result=>{
+                document.getElementById("error").innerHTML = "";
+                fetchData(page = cap);
+            }).catch(err=>{
+            document.getElementById("error").innerHTML = err;
+            });
+            document.getElementById("name").value = "";
+        }
+
---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const PORT = 3000;
+const app = express();
+
+// api for meals 
+// "https://www.themealdb.com/api/json/v1/1/random.php"
+
+const data=[];
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/api/:page",(request,response)=>{
+    const page = Number(request.params.page) - 1;
+    const list = data.slice(10*page,10*page+10);
+    response.json({list,size:parseInt(data.length/10)+1});
+})
+
+app.post("/name",(request,response)=>{
+    const {name}=request.body;
+    if(!name) return ;
+    const food = fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+    .then(res=>res.json())
+    .then(meal=>{
+        const food = meal.meals[0].strMeal;
+        data.push({name,food});
+        return response.json({name,food});
+    })
+    .catch(err=>console.log(err));
+})
+
+app.listen(PORT,()=>console.log(`...Server running on port ${PORT}`));
