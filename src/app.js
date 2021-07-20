02ad322b58496ccd626a5a5b15687a98adbbc1cf
AragoK7 <71692035+AragoK7@users.noreply.github.com>
2021-07-20 23:48:28 +0200

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const fetch = require("node-fetch");
-const PORT = env.process.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 // api for meals 
