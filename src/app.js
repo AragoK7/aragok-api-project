fe21e7b7b4d96de87f6c00707271445ae85b3737
AragoK <AragoK@example.com>
2021-08-26 15:11:43 +0200

    add database connection

--- src/app.js
+++ src/app.js
@@ -3,31 +3,63 @@ const fetch = require("node-fetch");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-// api for meals 
+// api for meals
 // "https://www.themealdb.com/api/json/v1/1/random.php"
 
-const data=[];
+const data = [];
+
+const { dbConnection } = require("./database.js");
+
+dbConnection.connect((err) => {
+  if (!err) {
+    console.log("Successfully connected to database");
+  } else {
+    throw err;
+  }
+});
 
 app.use(express.static("public"));
 app.use(express.json());
 
-app.get("/api/:page",(request,response)=>{
-    const page = Number(request.params.page) - 1;
-    const list = data.slice(10*page,10*page+10);
-    response.json({list,size:parseInt(data.length/10)+1});
-})
+app.get("/api/:page", async (request, response) => {
+  const skip =
+    Number(request.params.page) > 0
+      ? (Number(request.params.page) - 1) * 10
+      : 0;
+  const resultData = await dbConnection
+    .promise()
+    .query("SELECT sql_calc_found_rows * FROM names_meals LIMIT ?, 10", [skip]);
+  const list = resultData[0];
+  const resultCount = await dbConnection
+    .promise()
+    .query("SELECT found_rows() AS count;", [0]);
+  const count = resultCount[0][0].count;
+  const size = Math.ceil(count / 10);
+  console.log("count:", count);
+  response.json({ list, count, size });
+});
 
-app.post("/name",(request,response)=>{
-    const {name}=request.body;
-    if(!name) return ;
-    const food = fetch("https://www.themealdb.com/api/json/v1/1/random.php")
-    .then(res=>res.json())
-    .then(meal=>{
-        const food = meal.meals[0].strMeal;
-        data.push({name,food});
-        return response.json({name,food});
-    })
-    .catch(err=>console.log(err));
-})
+app.post("/name", async (request, response) => {
+  const { name } = request.body;
+  if (!name) return;
+  try {
+    const fetchResponse = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/random.php"
+    );
+    const data = await fetchResponse.json();
+    const food = data.meals[0].strMeal;
+    console.log(food);
+    const result = await dbConnection
+      .promise()
+      .query("INSERT INTO names_meals (username, meal) VALUES(?, ?)", [
+        name,
+        food,
+      ]);
+    response.json("should be good");
+  } catch (err) {
+    response.json("error");
+    response.json(err.message);
+  }
+});
 
-app.listen(PORT,()=>console.log(`...Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`...Server running on port ${PORT}`));
---
+++ src/database.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql2");
+
+const dbConnection = mysql.createConnection({
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+module.exports = {
+  dbConnection,
+};
